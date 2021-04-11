ab657bfe05a33a75107a0ac321f6c44da2ab63ef
Julien Grassick <julien.grassick@gmail.com>
2021-04-11 19:35:51 -0400

    Module

--- game.js
+++ game.js
@@ -1,3 +1,5 @@
+// import kaboom from './kaboom'
+
 // make kaboom functions global
 kaboom.global();
 
@@ -6,24 +8,43 @@ init({ fullscreen: true });
 
 loadSprite("tank-blue", "tank-blue.png");
 loadSprite("tank-red", "tank-red.png");
+loadSprite("missile", "missile.png")
 
 // define a scene
 scene("main", () => {
 
-  const wallL = add([
+  const walll = add([
     "wall",
     rect(10, height()),
     pos(0, 0),
     color(0, 0, 1),
     solid()
   ]);
+
   const wallr = add([
     "wall",
     rect(10, height()),
-    pos(width()-10, 0),
+    pos(width() - 10, 0),
     color(0, 0, 1),
     solid()
   ]);
+
+  const wallu = add([
+    "wall",
+    rect(width() - 20, 10),
+    pos(10, 0),
+    color(1, 0, 0),
+    solid()
+  ]);
+
+  const walld = add([
+    "wall",
+    rect(width() - 20, 10),
+    pos(10, height() - 10),
+    color(1, 0, 0),
+    solid()
+  ]);
+
   const blue = add([
     sprite("tank-blue"),
     pos(50, 50),
@@ -33,7 +54,7 @@ scene("main", () => {
   ])
 
   blue.action(() => {
-   blue.resolve()
+    blue.resolve()
   })
 
   const red = add([
@@ -46,34 +67,60 @@ scene("main", () => {
 
   red.action(() => {
     red.resolve()
-   })
+  })
 
   keyDown("a", () => {
-    red.angle += dt()*3
+    red.angle += dt() * 3
   })
+
   keyDown("s", () => {
-    red.move(-100 * Math.cos(red.angle), +100* Math.sin(red.angle))
+    red.move(-100 * Math.cos(red.angle), +100 * Math.sin(red.angle))
   })
 
   keyDown("d", () => {
-    red.angle -= dt()*3
+    red.angle -= dt() * 3
   })
 
-
- keyDown("4", () => {
-    blue.angle += dt()*3
+  keyDown("4", () => {
+    blue.angle += dt() * 3
   })
-
 
   keyDown("6", () => {
-    blue.angle -= dt()*3
+    blue.angle -= dt() * 3
   })
 
   keyDown("5", () => {
-    blue.move(100 * Math.cos(blue.angle), -100* Math.sin(blue.angle))
+    blue.move(100 * Math.cos(blue.angle), -100 * Math.sin(blue.angle))
+  })
+
+  keyPress("8", () => {
+    add([
+      sprite("missile"),
+      pos(blue.pos),
+      scale(0.1),
+      rotate(blue.angle),
+      origin("center"),
+      color(0.3, 0.3, 1),
+      "missile",
+    ])
+  })
+
+  action("missile", (obj) => {
+    obj.move(200 * Math.cos(obj.angle), -200 * Math.sin(obj.angle));
+  });
+
+  keyPress("w", () => {
+    add([
+      sprite("missile"),
+      pos(red.pos),
+      scale(0.1),
+      rotate(red.angle+Math.PI),
+      origin("center"),
+      color(1, 0.3, 0.3),
+      "missile",
+    ])
   })
 });
-
 
 
 // start the game
